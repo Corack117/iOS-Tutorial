faabcc873825b3a1cb698e385e23315337ba91fc
Ordaz Romero Sergio <orcheko@gmail.com>
2022-07-30 00:40:30 -0500

    Initial Commit

---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by Sergio Ordaz Romero on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
