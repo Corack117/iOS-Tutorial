e8ce953253933cbc0cca14cc4e72b6c84e567227
Ordaz Romero Sergio <orcheko@gmail.com>
2022-08-05 18:38:35 -0500

    Profile Views created

---
+++ Prueba/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Prueba
+//
+//  Created by Sergio Ordaz Romero on 04/08/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
