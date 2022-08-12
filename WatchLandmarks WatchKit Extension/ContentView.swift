df9a110cd2ec11fd219b61d2441a1e83ea8035cf
Ordaz Romero Sergio <orcheko@gmail.com>
2022-08-12 00:02:16 -0500

    watchOS application re-creation and termination

---
+++ WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Sergio Ordaz Romero on 11/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
