c02b310ac307fc8fb8cea15bc5b7888503d31454
Ordaz Romero Sergio <orcheko@gmail.com>
2022-08-12 00:45:59 -0500

    Add macOS target and define LandmarkDetail of macOS app

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Sergio Ordaz Romero on 12/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
